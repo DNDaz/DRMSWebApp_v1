6c2307fb3e09babbb406139f5ee1d802b31bd2de
dndaz <nkosinathi@syscon.co.za>
2018-02-21 07:53:21 +0200

    Finished Version 1

---
+++ source/src/main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import App from './App'
+import * as firebase from 'firebase'
+import router from './router'
+import { store } from './store'
+import DateFilter from './filters/date'
+import AlertCmp from './components/Shared/Alert.vue'
+import EditMeetupDetailsDialog from './components/Meetup/Edit/EditMeetupDetailsDialog.vue'
+import EditMeetupDateDialog from './components/Meetup/Edit/EditMeetupDateDialog.vue'
+import EditMeetupTimeDialog from './components/Meetup/Edit/EditMeetupTimeDialog.vue'
+import RegisterDialog from './components/Meetup/Registration/RegisterDialog.vue'
+import FirebaseConfig from './firebaseconfig.js'
+
+Vue.use(Vuetify)
+Vue.config.productionTip = false
+
+Vue.filter('date', DateFilter)
+Vue.component('app-alert', AlertCmp)
+Vue.component('app-edit-meetup-details-dialog', EditMeetupDetailsDialog)
+Vue.component('app-edit-meetup-date-dialog', EditMeetupDateDialog)
+Vue.component('app-edit-meetup-time-dialog', EditMeetupTimeDialog)
+Vue.component('app-meetup-register-dialog', RegisterDialog)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App),
+  created () {
+    console.log('on the created app hook' + FirebaseConfig)
+    console.log(FirebaseConfig)
+    firebase.initializeApp(
+      FirebaseConfig
+    )
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.$store.dispatch('autoSignIn', user)
+        this.$store.dispatch('fetchUserData')
+      }
+    })
+    this.$store.dispatch('loadMeetups')
+  }
+})
